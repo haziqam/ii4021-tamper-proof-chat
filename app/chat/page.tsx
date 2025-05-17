912461dd225839fb211a17021bceec74a406de71
Haziq Abiyyu Mahdy <haziqabiyyu@gmail.com>
2025-05-17 18:37:51 +0700

    feat: adjust docker-compose, add placeholders in UI

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,18 @@
-import { Chatbox } from './Chatbox'
+import { MessageSquareText } from 'lucide-react'
 
 export default async function ChatPage() {
-    // return <Chatbox />
-    return <></>
+    return (
+        <div className="w-full h-[100svh] flex justify-center items-center">
+            <ChatPagePlaceholder />
+        </div>
+    )
+}
+
+function ChatPagePlaceholder() {
+    return (
+        <div>
+            <MessageSquareText size={300} />
+            <div className="text-center text-2xl">Select a Conversation!</div>
+        </div>
+    )
 }
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,20 @@
+import { MessageSquarePlus } from 'lucide-react'
+
 export default async function SearchPage() {
-    return <div>Start a new conversation</div>
+    return (
+        <div className="w-full h-[100svh] flex justify-center items-center">
+            <SearchPagePlaceholder />
+        </div>
+    )
+}
+
+function SearchPagePlaceholder() {
+    return (
+        <div>
+            <MessageSquarePlus size={300} />
+            <div className="text-center text-2xl">
+                Start a New Conversation!
+            </div>
+        </div>
+    )
 }
